3b423d5b2ca7436f9df26953405b7588ee21220f
danabachulz <danabachulz@gmail.com>
2024-06-13 09:05:05 +0700

    update tesTes with datapoints

--- src/test/java/tesTest.java
+++ src/test/java/tesTest.java
@@ -1,8 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,10 +16,34 @@ import cofeeshop.CoffeeMachineFunctions;
 
 public class tesTest {
     CoffeeMachineFunctions cmf = new CoffeeMachineFunctions();
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        // Redirect System.out to the outputStreamCaptor
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        // Reset System.out to its original stream
+        System.setOut(originalOut);
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {1,2,3,4,5,6,7,8,9,})
     public void coba_espresso(int cup){
-        cmf.espresso(cup);
+        
+        CoffeeMachineFunctions.espresso(cup);;
+
+        String expectedOutput = "\n Starting to make coffee" +
+                        "\n Grinding coffee beans" +
+                        "\n Boiling water" +
+                        "\n Mixing boiled water with crushed coffee beans" +
+                        "\n Pouring coffee into the cup" +
+                        "\n Coffee is ready!\n";
+        // Verify the output
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
     }
 }
